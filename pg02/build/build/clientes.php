0935d7ce556fe833970963b91a1c1a6e40222dec
matheusdorsi <102078873+matheusdorsi@users.noreply.github.com>
2022-07-05 04:40:01 -0300

    Add files via upload

---
+++ pg02/build/build/clientes.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	  <meta https-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
+</head>
+<body>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript"  src="js/clientsStickers.js"></script>
+<div class="features">
+	<div class="features-content free" id='content'>
+</div>
+</div>
+<script>
+	$("#content").runSticker();
+</script>
+<img src='https://app.monetizze.com.br/r/AXW1950069/?u=DL23462' width='1' height='1'></body>
+</html>
